55562ab9d373355affcd8d47974da81cf739de3c
DahlinOliver <oliver.dahlin@student.kyh.se>
2020-09-19 01:49:31 +0200

    Added functions for DEL button and % button

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ function pushBtn(obj) {
         // Clear calculator
         inputLabel.innerHTML = '0';
 
+    } else if (pushed == 'DEL') {
+        // Deletes latest event
+        inputLabel.innerHTML = inputLabel.innerHTML.slice(0, -1);
+
+        if (inputLabel.innerHTML.length < 1) {
+            inputLabel.innerHTML = '0';
+        }
+
+    } else if (pushed == '%') {
+        // Divides number to percent
+        inputLabel.innerHTML = inputLabel.innerHTML / 100;
+
     } else {
         if (inputLabel.innerHTML == '0') {
             inputLabel.innerHTML = pushed;
